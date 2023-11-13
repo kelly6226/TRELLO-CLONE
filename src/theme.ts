56f07caf156d3cc887cfd7430071a5851ec4f5dc
jisooshin <kelly6226@naver.com>
2023-11-13 21:50:03 +0900

    [FEAT] #7.4 Styles and Placeholders

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const darkTheme: DefaultTheme = {
   bgColor: "#3F8CF2",
   boardColor: "#DADFE9",
-  cardColor: "#white",
+  cardColor: "white",
 };
